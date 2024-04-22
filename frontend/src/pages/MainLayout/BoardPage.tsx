72717473dd0ff7924f3e596e1b3028a86ae85a7a
ThinhTD <tranducthinh.97@gmail.com>
2024-04-23 00:36:39 +0700

    [KAN-3] Rev UI props

--- frontend/src/pages/MainLayout/BoardPage.tsx
+++ frontend/src/pages/MainLayout/BoardPage.tsx
@@ -17,20 +17,6 @@ const BoardPage = () => {
       <button className="bg-sky-400" onClick={logOutHandler}>
         Log Out
       </button>
-      <TaskCard
-        taskId="1"
-        issueKey="KAN-5"
-        title="Header - UI"
-        description="123"
-        status={ETaskStatus.done}
-        dueDate={1713484800000}
-        assigneeId={['1', '2', '3']}
-        createBy="1232"
-        createAt={1713484800000}
-        updatedAt={1713484800000}
-        issueLink="#"
-        relatedCommit="#asdfasdf"
-      />
     </>
   );
 };
